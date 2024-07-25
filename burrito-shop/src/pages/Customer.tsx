3b6db67e750ae1512dca77ff247d6b8a058d64a3
Juliana Soranno <julianasoranno@10-17-97-176.dynapool.wireless.nyu.edu>
2024-07-25 17:32:36 -0400

    vercel

--- burrito-shop/src/pages/Customer.tsx
+++ burrito-shop/src/pages/Customer.tsx
@@ -1,19 +1,18 @@
-import burritos from "../api/burrito.json"
-import { Col, Row } from "react-bootstrap"
-import { ShopItem } from "../components/ShopItem"
+import burritos from "../api/burrito.json";
+import { Col, Row } from "react-bootstrap";
+import { ShopItem } from "../components/ShopItem";
 
-export function Customer(){
-    return (
+export function Customer() {
+  return (
     <>
-        <h1 className="heading">Burritos</h1>
-        < Row md={2} xs={1} lg = {3} className = "g-3">
-            {burritos.map(burrito => 
-                <Col key={burrito.id}>
-                    <ShopItem {...burrito} />
-                </Col>
-            )}
-            
-        </Row>
+      <h1 className="heading">Burritos</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {burritos.map((burrito) => (
+          <Col key={burrito.id}>
+            <ShopItem {...burrito} />
+          </Col>
+        ))}
+      </Row>
     </>
-    )
+  );
 }
